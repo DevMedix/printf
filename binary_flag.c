16fef7170372456ade53de521945add4514b7887
DevMedix <rawlingsmichael01@gmail.com>
2023-04-25 11:40:30 +0000

    check for binary

---
+++ binary_flag.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+/**
+ * _binary_conversion - validates binaries
+ * @str: format string
+ * Return: 1 if checks else 0
+ */
+int _binary_conversion(char *str)
+{
+	(void) str;
+
+	return (1);
+}
+
+
+
+/**
+ * _create_binary - make an octal string
+ * @str: a format string
+ * @valist: va_list variable
+ * Return: returns pointer to the result
+ */
+char *_create_binary(char *str, va_list valist)
+{
+	unsigned int num;
+	char *res;
+	(void) str;
+
+	num = va_arg(valist, unsigned int);
+	res = octorbi(num, 2);
+	return (res);
+}
+
