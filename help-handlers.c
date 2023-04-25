b611d8cc6cac22155cfea1f539cfeeba0be9d511
Gmukoro <gmukoro3@gmail.com>
2023-04-25 12:37:33 -0700

    updated

--- help-handlers.c
+++ help-handlers.c
@@ -3,101 +3,101 @@
 #include <stdlib.h>
 
 /**
- * format_precision - Returns the right format precision
- * @s: The format string;
- * @format: Format character
- * Return: Precision numb of -1 if it does not exist
+ * format_precision - A funct that returns the appropriate precision
+ * @s: format string;
+ * @formatter: formatter
+ * Return: precision number of -1 if it doesnt exist
  */
 
-int format_precision(char *s, char format)
+int format_precision(char *s, char formatter)
 {
-	int t, i;
+	int p, i;
 
-	t = 0;
-	for (i = 0; s[i] != format; i++)
+	p = 0;
+	for (i = 0; s[i] != formatter; i++)
 	{
 		if (s[i] == '.')
 		{
 			i++;
-			while (s[i] != format)
+			while (s[i] != formatter)
 			{
-				t = 10 * t + s[i] - '0';
+				p = 10 * p + s[i] - '0';
 				i++;
 			}
-			if (t == 0)
+			if (p == 0)
 				return (-1);
 			break;
 		}
 	}
-	return (t);
+	return (p);
 }
 
 /**
- * width_handler - Returns the correct width
- * @s: The format string;
- * @format: format of str to check
- * Return: width or -1 if width does not exist
+ * width_handler - A funct that returns the appropriate width
+ * @s: format string;
+ * @formatter: formatter to check
+ * Return: width or -1 if width doesnt exist
  */
 
-int width_handler(char *s, char format)
+int width_handler(char *s, char formatter)
 {
-	int width, i;
+	int w, i;
 
 	i = 1;
-	width = 0;
-	while ((s[i] != '.' || s[i] != format) && _is_digit(s[i]))
+	w = 0;
+	while ((s[i] != '.' || s[i] != formatter) && _is_digit(s[i]))
 	{
-		width = 10 * width + s[i] - '0';
+		w = 10 * w + s[i] - '0';
 		i++;
 	}
-	return (width);
+	return (w);
 }
 
 /**
- * ptr_result- Returns a pointer back to results
- * @arr: Array
- * @s: String
- * @pres: Precision
- * @width: Width
- * @slen: String length
- * @flen: format length
- * @mlen: Malloc length
- * Return: Pointer to array result
+ * ptr_result - A that gives a pointer back to results
+ * @r: array
+ * @s: string
+ * @p: precision
+ * @w: width
+ * @slen: str len
+ * @flen: format len
+ * @mlen: malloc len
+ * Return: result pointer
  */
 
-char *ptr_result(char *arr, char *s, int pres, int width
-, int slen, int flen, int mlen)
+char *ptr_result(char *r, char *s, int p, int w, int slen, int flen, int mlen)
 {
 	int t, k, i;
 
 	t = k = i = 0;
 
-	if (pres > 0 && width > 0)
+	if (p > 0 && w > 0)
 	{
-		for (t = 0; t < (slen - pres); t++)
-			arr[t] = ' ';
-		for (k = 0; k < pres; k++, t++)
-			arr[t] = s[k];
-		arr[t] = '\0';
+		for (t = 0; t < (slen - p); t++)
+			r[t] = ' ';
+		for (k = 0; k < p; k++, t++)
+			r[t] = s[k];
+		r[t] = '\0';
 	}
-	else if (pres == 0 && width == 0 && flen == 2)
+
+	else if (p == 0 && w == 0 && flen == 2)
 	{
 		for (t = 0; s[t] != '\0'; t++)
-			arr[t] = s[t];
-		arr[t] = '\0';
+			r[t] = s[t];
+		r[t] = '\0';
 	}
-	else if (pres == -1)
+
+	else if (p == -1)
 	{
-		arr[t] = '\0';
+		r[t] = '\0';
 	}
 	else
 	{
-		for (t = 0; t < (width - slen); t++)
-			arr[t] = ' ';
+		for (t = 0; t < (w - slen); t++)
+			r[t] = ' ';
 		for (k = k + t, i = 0; k < mlen && s[i] != '\0'; k++, i++)
-			arr[k] = s[i];
-		arr[k] = '\0';
+			r[k] = s[i];
+		r[k] = '\0';
 	}
-	return (arr);
-
+	return (r);
 }
