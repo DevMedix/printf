5166b3d5ee4be963310f15e76a67b07a3e4b1d34
DevMedix <rawlingsmichael01@gmail.com>
2023-04-25 11:39:52 +0000

    added new prototypes

--- main.h
+++ main.h
@@ -1,20 +1,45 @@
-#ifndef HEADER
-#define HEADER
-#include <stdarg.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/*macro definition*/
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <limits.h>
+
+/* ---------- MACROS ---------- */
 #define BUF_LENGTH 1024
 
+/* ---------- PROTOTYPE FOR BINARY ---------- */
+int _binary_conversion(char *str);
+char *_create_binary(char *str, va_list valist);
 
+/* ---------- PROTOTYPE FOR BUFFER ---------- */
+char *_buffer_flush(char *buffer);
+char *_buff_fill(char *buffer, const char *s, int count_c, int s_length);
+void _write_buffer(char *buffer, int len);
 
+/* ---------- PROTOTYPE FOR CHAR ---------- */
+char *_create_char(char *str, va_list valist);
+int _convert_char(char *str);
 
+/* ---------- PROTOTYPE FOR DECIMAL ----------*/
+char *_create_decimal(char *str, va_list valist);
+int _convert_di(char *str);
+char *_aoti(int num);
 
+/* ---------- PROTOTYPE FOR FORMAT HANDLER ---------- */
+char *_get_format(const char *fmt);
+void _format_filler(const char *fmt);
 
+/* ---------- PROTOTYPE FOR GET_VALID_FUN && GET_CHE_STR_FUNC ---------- */
+int (*get_valid_func(char c))(char *);
+char *(*get_che_str_func(char c))(char *, va_list);
 
-
-
-
-
+/* ---------- PROTOTYPE FOR UPPERCASE HEX ---------- */
+char *_create_hex_upper(char *str, va_list valist);
+char *_hex_upper(unsigned int n);
 
 
 
@@ -24,7 +49,7 @@
 /*structs*/
 /**
  * struct valid - Checks for formatting Valiity
- * @type: Type 
+ * @type: Type
  * @conver_check: Funct to check for str conversion validity
  */
 typedef struct valid
@@ -45,4 +70,4 @@ typedef struct che_str
 } che_str;
 
 
-#endif
+#endif /* MAIN_H */
