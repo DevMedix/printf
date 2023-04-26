88f76f4e245d07b506282283b1edbe2fd76c361d
Gmukoro <gmukoro3@gmail.com>
2023-04-25 17:06:12 -0700

    unsigned_octal_flag.c

--- unsigned_octal_flag.c
+++ unsigned_octal_flag.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _print_octal_flag - prints unsigned octal
- * @valist: va_list variable
- * @buf: buffer
- * @f: flag
- * @w: width
- * @p: precision
- * @s: size
+ * _print_octal_flag - A funct that prints unsigned octal
+ * @valist: Va_list variable
+ * @buf: Buffer
+ * @f: Flag
+ * @w: Width
+ * @p: Precision
+ * @s: Size
  *
- * Return: int
+ * Return: Integer
  */
 int _print_octal_flag(va_list valist, char buf[], int f, int w, int p, int s)
 {
